ba4afee4ee5ef70dc9daf4f7a9b00b98c62ed194
b0n541 <j@nschaefer.net>
2024-12-01 16:32:08 +0100

    Add solution for day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,57 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+
+        val list1 = mutableListOf<Int>()
+        val list2 = mutableListOf<Int>()
+
+        input.map { it -> it.split("   ") }
+            .forEach { it ->
+                list1.add(it[0].toInt())
+                list2.add(it[1].toInt())
+            }
+
+        list1.sort()
+        list2.sort()
+
+        var result = 0
+        for (i in list1.indices) {
+            result += abs(list1[i] - list2[i])
+        }
+
+        return result
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val list1 = mutableListOf<Int>()
+        val list2 = mutableListOf<Int>()
+
+        input.map { it -> it.split("   ") }
+            .forEach { it ->
+                list1.add(it[0].toInt())
+                list2.add(it[1].toInt())
+            }
+
+        var result = 0
+        for (i in list1.indices) {
+            result += list1[i] * list2.count { it -> it == list1[i] }
+        }
+
+        return result
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testResult1 = part1(testInput)
+    println("Test result part 1: $testResult1")
+    check(testResult1 == 11)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    println("Solution part 1:    ${part1(input)}")
+
+    val testResult2 = part2(testInput)
+    println("Test result part 2: $testResult2")
+    check(testResult2 == 31)
+
+    println("Solution part 1:    ${part2(input)}")
 }
